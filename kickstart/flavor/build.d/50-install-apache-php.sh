9ea6abce0d166faf66b1c7f775eea4edce564006
Matthias Leuffen <matthes@leuffen.de>
2022-10-22 01:31:29 +0200

    update

--- kickstart/flavor/build.d/50-install-apache-php.sh
+++ kickstart/flavor/build.d/50-install-apache-php.sh
@@ -17,10 +17,13 @@ curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
 sudo apt-get install -y nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn esbuild
+sudo apt-get update && sudo apt-get install yarn
 # Install NPM
 #apt-get -y install --no-install-recommends npm
 #npm install --global webpack-cli typescript ts-loader
+
+# Install EsBuild JS/CSS Bundler
+sudo apt-get install -y esbuild
 
 ## Install Composer2
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php

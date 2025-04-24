c13360cc6684082d3a4383875ea4a212286dce9b
Matthias <m@tth.es>
2025-04-24 23:27:34 +0200

    sync

--- kickstart/flavor/build.d/50-install-apache-php.sh
+++ kickstart/flavor/build.d/50-install-apache-php.sh
@@ -9,7 +9,7 @@ apt-get install -y software-properties-common
 apt-get update
 
 
-apt-get -y install gettext apache2 libapache2-mod-php8.3 php8.3-xdebug php8.3-mbstring php8.3-yaml\
+apt-get -y install gettext openssh-server apache2 libapache2-mod-php8.3 php8.3-xdebug php8.3-mbstring php8.3-yaml\
                    iputils-ping telnet composer esbuild inotify-tools \
                   php8.3-bcmath php8.3-bz2 php8.3-curl php8.3-exif php8.3-ftp php8.3-gd php8.3-gettext php8.3-gmp php8.3-http php8.3-igbinary php8.3-imagick php8.3-imap php8.3-intl php8.3-ldap php8.3-memcached php8.3-mongodb php8.3-msgpack php8.3-mysql php8.3-odbc php8.3-pspell php8.3-raphf php8.3-readline php8.3-redis php8.3-snmp php8.3-soap php8.3-sockets php8.3-sqlite3 php8.3-ssh2 php8.3-tidy php8.3-xml php8.3-xmlrpc php8.3-xsl php8.3-zip
 # ssh2 is used for composer private repositories
@@ -19,7 +19,7 @@ apt-get -y install gettext apache2 libapache2-mod-php8.3 php8.3-xdebug php8.3-mb
 # Install Node and NPM - See https://github.com/nodesource/distributions?tab=readme-ov-file#ubuntu-versions
 curl -sL https://deb.nodesource.com/setup_23.x | sudo bash -
 sudo apt-get install -y nodejs
-sudo npm install -g npm
+sudo npm install -g npm yarn pnpm
 
 # npm removed
 
@@ -43,3 +43,7 @@ echo ". /home/user/.bashrc.kickstart" >> /home/user/.bashrc
 
 ## Ubuntu 23.04 -> change user ubuntu to uid 999
 usermod -u 999 ubuntu
+
+## configure sshd to allow empty passwords for user (to allow remote debugging for node, vite, etc.)
+echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
--- kickstart/run/start.d/10-start-apache.sh
+++ kickstart/run/start.d/10-start-apache.sh
@@ -8,6 +8,9 @@ if [[ "$DEV_MODE" = "0" ]]
 then
     phpdismod xdebug
 else
+    # Start sshd
+    service ssh start
+    # Enable xdebu
     phpenmod xdebug
     echo 'export PHP_IDE_CONFIG="serverName=$DEV_CONTAINER_NAME"' >> /home/user/.bashrc.kickstart
     echo 'export XDEBUG_CONFIG="idekey=$DEV_CONTAINER_NAME"' >> /home/user/.bashrc.kickstart
